a9e803096ce22cfeb672395c9775f024749e827e
Leonardo Razovic <leorazzo@gmail.com>
2020-02-20 11:17:00 +0100

    remove unused dependencies

--- encoder.py
+++ encoder.py
@@ -1,19 +1,15 @@
 from tensorflow import keras
-from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D, BatchNormalization
-from tensorflow.keras.models import Model
-from tensorflow.keras import backend as K
-from tensorflow.keras.datasets import mnist
-import numpy as np
-from tensorflow.keras.callbacks import TensorBoard
-import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, BatchNormalization
+
 
 def model_encoder(input_imgs, input_messages):
 
     [N, C, H, W] = input_imgs.size()
 
-    #Phase 1
+    # Phase 1
 
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', strides=1)(input_imgs)
+    x = Conv2D(64, (3, 3), activation='relu',
+               padding='same', strides=1)(input_imgs)
     x = BatchNormalization(axis=1)(x)
     x = Conv2D(64, (3, 3), activation='relu', padding='same', strides=1)(x)
     x = BatchNormalization(axis=1)(x)
@@ -22,10 +18,10 @@ def model_encoder(input_imgs, input_messages):
     x = Conv2D(64, (3, 3), activation='relu', padding='same', strides=1)(x)
     x = BatchNormalization(axis=1)(x)
 
-    #Phase 2
+    # Phase 2
 
-    #Here I'm concateneting msg, original image and conv_image from the previous layer
-    #At the end of the for x_batch will contain all the images concatened
+    # Here I'm concateneting msg, original image and conv_image from the previous layer
+    # At the end of the for x_batch will contain all the images concatened
 
     for i in range(N):
         msg = input_messages[i].repeat(H, W, 1).permute(2, 0, 1)
@@ -38,27 +34,11 @@ def model_encoder(input_imgs, input_messages):
         else:
             x_batch = tf.concat([x_batch, x2], 0)
 
+    # Phase 3
 
-    #Phase 3
-
-    encoded_images = Conv2D(64, (3, 3), activation='relu', padding='same', strides=1)(x_batch)
-    encoded_images = Conv2D(C, (1, 1), padding='same', strides=1)(encoded_images)
+    encoded_images = Conv2D(64, (3, 3), activation='relu',
+                            padding='same', strides=1)(x_batch)
+    encoded_images = Conv2D(C, (1, 1), padding='same',
+                            strides=1)(encoded_images)
 
     return encoded_images
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
