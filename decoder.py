5e23a4576d65aa4f78eff530231c82b02b995f82
federicoInserra <federico.inserra96@gmail.com>
2020-02-20 11:36:40 +0100

    rename input images in encoded images

--- decoder.py
+++ decoder.py
@@ -3,34 +3,34 @@ from tensorflow.keras.layers import Conv2D, BatchNormalization,\
     AveragePooling2D
 
 
-def model_decoder(input_imgs, input_message):
+def model_decoder(encoded_imgs, input_message):
 
-    [N, C, H, W] = input_imgs.size()
+    [N, C, H, W] = encoded_imgs.size()
     message_len = len(input_message)
 
     x = Conv2D(64, (3, 3), activation='relu',
-               padding='same', strides=1)(input_imgs)
+               padding='same', strides=1)(encoded_imgs)
     x = BatchNormalization(axis=1)(x)
     x = Conv2D(64, (3, 3), activation='relu',
-               padding='same', strides=1)(input_imgs)
+               padding='same', strides=1)(encoded_imgs)
     x = BatchNormalization(axis=1)(x)
     x = Conv2D(64, (3, 3), activation='relu',
-               padding='same', strides=1)(input_imgs)
+               padding='same', strides=1)(encoded_imgs)
     x = BatchNormalization(axis=1)(x)
     x = Conv2D(64, (3, 3), activation='relu',
-               padding='same', strides=1)(input_imgs)
+               padding='same', strides=1)(encoded_imgs)
     x = BatchNormalization(axis=1)(x)
     x = Conv2D(64, (3, 3), activation='relu',
-               padding='same', strides=1)(input_imgs)
+               padding='same', strides=1)(encoded_imgs)
     x = BatchNormalization(axis=1)(x)
     x = Conv2D(64, (3, 3), activation='relu',
-               padding='same', strides=1)(input_imgs)
+               padding='same', strides=1)(encoded_imgs)
     x = BatchNormalization(axis=1)(x)
     x = Conv2D(64, (3, 3), activation='relu',
-               padding='same', strides=1)(input_imgs)
+               padding='same', strides=1)(encoded_imgs)
     x = BatchNormalization(axis=1)(x)
     x = Conv2D(message_length, (3, 3), activation='relu',
-               padding='same', strides=1)(input_imgs)
+               padding='same', strides=1)(encoded_imgs)
     x = BatchNormalization(axis=1)(x)
     output_message = AveragePooling2D(pool_size=(message_len, message_len),
                                       strides=1,
