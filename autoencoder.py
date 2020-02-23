0da6d7bca398b9c1cbf0e90852507746a9a5b1d8
Leonardo Razovic <leorazzo@gmail.com>
2020-02-23 17:08:05 +0100

    new autoencoder model

---
+++ autoencoder.py
@@ -0,0 +1,132 @@
+from tensorflow.keras.layers import Activation, Dense, Input, BatchNormalization
+from tensorflow.keras.layers import Conv2D, Flatten
+from tensorflow.keras.layers import Reshape, Conv2DTranspose
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+from tensorflow.keras.datasets import mnist
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# MNIST dataset
+(x_train, _), (x_test, _) = mnist.load_data()
+
+image_size = x_train.shape[1]
+x_train = np.reshape(x_train, [-1, image_size, image_size, 1])
+x_test = np.reshape(x_test, [-1, image_size, image_size, 1])
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# Generate corrupted MNIST images by adding noise with normal dist
+# centered at 0.5 and std=0.5
+noise = np.random.normal(loc=0.5, scale=0.5, size=x_train.shape)
+x_train_noisy = x_train + noise
+noise = np.random.normal(loc=0.5, scale=0.5, size=x_test.shape)
+x_test_noisy = x_test + noise
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+# Network parameters
+input_shape = (image_size, image_size, 1)
+batch_size = 128
+kernel_size = 3
+latent_dim = 16
+# Encoder/Decoder number of CNN layers and filters per layer
+layer_filters = [64, 64, 64, 64, 64]
+
+# Build the Autoencoder Model
+# First build the Encoder Model
+inputs = Input(shape=input_shape, name='encoder_input')
+x = inputs
+# Stack of Conv2D blocks
+# Notes:
+# 1) Use Batch Normalization before ReLU on deep networks
+# 2) Use MaxPooling2D as alternative to strides>1
+# - faster but not as good as strides>1
+for filters in layer_filters:
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               strides=1,
+               padding='same')(x)
+    x = BatchNormalization(axis=1)(x)
+    x = Activation("relu")(x)
+x = Conv2D(1, 1, padding='same', strides=1)(x)
+# Shape info needed to build Decoder Model
+shape = K.int_shape(x)
+
+# Generate the latent vector
+x = Flatten()(x)
+latent = Dense(latent_dim, name='latent_vector')(x)
+
+# Instantiate Encoder Model
+encoder = Model(inputs, latent, name='encoder')
+encoder.summary()
+
+# Build the Decoder Model
+latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
+x = Dense(shape[1] * shape[2] * shape[3])(latent_inputs)
+x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+# Stack of Transposed Conv2D blocks
+# Notes:
+# 1) Use Batch Normalization before ReLU on deep networks
+# 2) Use UpSampling2D as alternative to strides>1
+# - faster but not as good as strides>1
+
+decoder_filters = [64, 64, 64, 64, 64, 64, 64]
+
+for filters in decoder_filters:
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               strides=1,
+               padding='same')(x)
+    x = BatchNormalization(axis=1)(x)
+    x = Activation("relu")(x)
+
+x = Conv2D(filters=1,
+           kernel_size=kernel_size,
+           padding='same')(x)
+# x = Conv2D(message_length, (3, 3), activation='relu',
+#                padding='same', strides=1)(x)
+x = BatchNormalization(axis=1)(x)
+outputs = Activation('sigmoid', name='decoder_output')(x)
+
+# Instantiate Decoder Model
+decoder = Model(latent_inputs, outputs, name='decoder')
+decoder.summary()
+
+# Autoencoder = Encoder + Decoder
+# Instantiate Autoencoder Model
+autoencoder = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+autoencoder.summary()
+
+autoencoder.compile(loss='mse', optimizer='adam')
+
+# Train the autoencoder
+autoencoder.fit(x_train_noisy,
+                x_train,
+                validation_data=(x_test_noisy, x_test),
+                epochs=30,
+                batch_size=batch_size)
+
+# Predict the Autoencoder output from corrupted test images
+x_decoded = autoencoder.predict(x_test_noisy)
+
+# Display the 1st 8 corrupted and denoised images
+rows, cols = 10, 30
+num = rows * cols
+imgs = np.concatenate([x_test[:num], x_test_noisy[:num], x_decoded[:num]])
+imgs = imgs.reshape((rows * 3, cols, image_size, image_size))
+imgs = np.vstack(np.split(imgs, rows, axis=1))
+imgs = imgs.reshape((rows * 3, -1, image_size, image_size))
+imgs = np.vstack([np.hstack(i) for i in imgs])
+imgs = (imgs * 255).astype(np.uint8)
+plt.figure()
+plt.axis('off')
+plt.title('Original images: top rows, '
+          'Corrupted Input: middle rows, '
+          'Denoised Input:  third rows')
+plt.imshow(imgs, interpolation='none', cmap='gray')
+Image.fromarray(imgs).save('corrupted_and_denoised.png')
+plt.show()
