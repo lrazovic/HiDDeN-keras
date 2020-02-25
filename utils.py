ab8052babb74f606ae1fe0b9290c78bea86d116b
Leonardo Razovic <leorazzo@gmail.com>
2020-02-25 18:59:53 +0100

    message generation

--- utils.py
+++ utils.py
@@ -1,3 +1,19 @@
+import numpy as np
+
+
 def string_to_binary(string):
     # UTF-8 Encoding
     return ' '.join(format(ord(x), 'b') for x in string)
+
+
+def generate_random_messages(N):
+    messages = list()
+    for _ in range(N):
+        s = []
+        text = "Hello, World!"
+        binary_message = string_to_binary(text)
+        for c in binary_message:
+            s.append(float(c))
+        s = np.asarray(s)
+        messages.append(s)
+    return np.asarray(messages)
