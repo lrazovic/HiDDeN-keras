8322df55b26a46e32c0aba8594a53f9eab9681b8
Leonardo Razovic <leorazzo@gmail.com>
2020-03-04 09:44:51 +0100

    GlobalAveragePooling2D on decoder

--- decoder.py
+++ decoder.py
@@ -4,12 +4,11 @@ from tensorflow.keras.models import Model
 from const import *
 
 
-def model_decoder(latent_inputs, decoder_filters, shape, L):
-    x = Dense(shape[1] * shape[2] * shape[3])(latent_inputs)
-    x = Reshape((shape[1], shape[2], shape[3]))(x)
-
+def model_decoder(encoded_images, decoder_filters, L):
+    # 7 ConvBnReLU
+    x = encoded_images
     for filters in decoder_filters:
-        x = Conv2D(filters=filters,
+        x = Conv2D(filters,
                    kernel_size=KERNEL_SIZE,
                    strides=1,
                    padding='same')(x)
@@ -17,13 +16,14 @@ def model_decoder(latent_inputs, decoder_filters, shape, L):
         x = Activation("relu")(x)
 
     # Last ConvBNReLU with L filters
-    x = Conv2D(filters=L,
+    x = Conv2D(L,
                kernel_size=KERNEL_SIZE,
                padding='same')(x)
     x = BatchNormalization(axis=1)(x)
     x = Activation("relu")(x)
 
-    #x = GlobalAveragePooling2D()(x)
+    x = GlobalAveragePooling2D()(x)
+
     outputs = Activation('sigmoid', name='decoder_output')(x)
-    decoder = Model(latent_inputs, outputs, name='decoder')
-    return decoder
+    # decoder = Model(encoded_images, outputs, name='decoder')
+    return outputs
