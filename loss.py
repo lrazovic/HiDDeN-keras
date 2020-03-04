48d8daf5b186910fa37539dc374f4697e76dfdf1
Leonardo Razovic <leorazzo@gmail.com>
2020-03-04 09:46:20 +0100

    message distortion loss in order to have a better prediction

--- loss.py
+++ loss.py
@@ -1,5 +1,9 @@
 from tensorflow.keras.backend import sqrt, sum, square
 
 
-def euclidean_distance_loss(y_true, y_pred):
-    return sqrt(sum(square(y_pred - y_true), axis=-1))
+def image_distortion_loss(y_true, y_pred):
+    return sqrt(sum(square(y_pred - y_true), axis=1))/28*28
+
+
+def message_distortion_loss(y_true, y_pred):
+    return sqrt(sum(square(y_pred - y_true), axis=1))/96
